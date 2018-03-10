7574f94bb351428ca25ec4870dab8fafa3cd6c52
Yaroslav Draga <yaroslav@spheremall.com>
2018-03-10 10:50:23 +0200

    mognod add update script

--- src/services/mongodb-find.js
+++ src/services/mongodb-find.js
@@ -7,10 +7,22 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, client) => {
     console.log('Connected to MongoDB server');
     const db = client.db('TodoApp');
 
-    db.collection('Todos').findOneAndDelete({complete: false})
-        .then((res) => {
-           console.log(res);
-        });
+    db.collection('Users').findOneAndUpdate(
+        {_id: new ObjectID('5aa2e0b8bef0bb28885645aa')},
+        {
+            $set: {
+                name: "Yaroslav 2"
+            },
+            $inc: {
+                aga: 1
+            }
+        },
+        {
+            returnOriginal: false
+        }
+    ).then((res) => {
+        console.log(res);
+    });
 
     client.close();
 });
