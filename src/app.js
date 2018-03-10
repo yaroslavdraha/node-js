56c7668991941c46f01b26d94d08fb2f4696cdcd
Yaroslav Draga <yaroslav@spheremall.com>
2018-03-10 21:01:00 +0200

    Add tests for POST /todos

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 
 const mongoose = require('./services/mongoose');
-const User = require('./models/user');
 const Todo = require('./models/todo');
 
 const app = new express();
--- test/app.test.js
+++ test/app.test.js
@@ -5,38 +5,59 @@ let {expect, assert} = require('chai');
 
 //region [Module require]
 const app = require('../src/app');
+const Todo = require('../src/models/todo');
 //endregion
 
-describe('Application', function () {
-    describe('GET /', function () {
-        it('should return information about Sashko', function (done) {
-            request(app)
-                .get('/')
-                .expect(200)
-                .expect((res) => {
-                    const body = res.body;
-                    expect(body).to.include({
-                        name: "Sashko"
-                    });
-                })
-                .end(done);
+beforeEach((done) => {
+  Todo.remove({}).then(() => done());
+});
+
+describe('Application', () => {
+  describe('POST /todos', () => {
+    it('should add new Todo', (done) => {
+      let text = 'Test Todo text';
+
+      request(app)
+        .post('/todos')
+        .send({text})
+        .expect(200)
+        .expect(res => {
+          expect(res.body.text).to.equal(text);
+        })
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          }
+
+          Todo.find().then(todos => {
+            expect(todos).to.have.lengthOf(1);
+            expect(todos[0].text).to.equal(text);
+
+            done();
+          }).catch((e) => done(e));
         });
     });
 
-    describe('GET /users', function () {
-        it('should return Users (/users)', function (done) {
-            request(app)
-                .get('/users')
-                .expect(200)
-                .expect((res) => {
-                    const body = res.body;
-                    expect(body).to.be.an('array');
-                    expect(body).to.deep.include({
-                        name: "Yaroslav",
-                        age: 22
-                    });
-                })
-                .end(done);
+    it('should not create Todo with invalid body data', (done) => {
+      request(app)
+        .post('/post')
+        .send({})
+        .expect(404)
+        .expect(res => {
+          expect(Object.keys(res.body)).to.be.lengthOf(0);
+        })
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          }
+
+          Todo.find().then(todos => {
+            expect(todos).to.have.lengthOf(0);
+            done();
+          }).catch((err) => {
+            done(err);
+          });
         });
     });
+  });
 });
