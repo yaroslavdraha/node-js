86a90ce632c8c1c0c0339e9063ac356e63780ade
Yaroslav Draga <yaroslav@spheremall.com>
2018-03-10 21:21:28 +0200

    Add get todos route

--- src/app.js
+++ src/app.js
@@ -15,13 +15,17 @@ app.post('/todos', (req, res) => {
 
   todo.save().then((doc) => {
     res.send(doc);
-  }, (e) => {
-    res.status(400).send(e);
-  })
+  }, (e) => res.status(400).send(e))
+});
+
+app.get('/todos', (req, res) => {
+  Todo.find().then(todos => {
+    res.send({todos});
+  }, (e) => res.status(400).send(e));
 });
 
 app.listen(port, () => {
-    console.log(`Server is up on ${port}`);
+  console.log(`Server is up on ${port}`);
 });
 
 module.exports = app;
