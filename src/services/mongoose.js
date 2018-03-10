f5406598b0c659980247c5a1528c17f9c0872023
Yaroslav Draga <yaroslav@spheremall.com>
2018-03-10 23:06:20 +0200

    MongoDB heroku config

--- src/services/mongoose.js
+++ src/services/mongoose.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+console.log(process.env);
+
+const mongoUrl = process.env.NODE_ENV === 'production'
+  ? 'mongodb://yaroslav:[eq[ekb123@ds135680.mlab.com:35680/mean-app-db'
+  : 'mongodb://localhost:27017/TodoApp';
+
+mongoose.connect(mongoUrl)
+  .then(() => {}, (err) => {console.log(err)});
 
 module.exports = mongoose;
